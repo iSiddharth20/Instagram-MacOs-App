23a6d5e77f8751b3e6ac8943c5003ab4b0a66d2b
Siddharth Kekre <siddharthkekre@iSiddharthMac.local>
2020-08-18 23:02:06 +0530

    Basic Changes

--- Appify UI Node Demo.app/Contents/Resources/app/lib/webview.js
+++
@@ -1,7 +0,0 @@
-var spawn = require('child_process').spawn;
-
-exports.open = function(url){
-  return spawn(__dirname + '/../../../MacOS/appify-ui-webview', ['-url', url]);
-}
-
-if (!module.parent) exports.open('http://m.facebook.com/');
